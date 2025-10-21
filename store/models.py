79084cc2896975fcfbc65c5b8c6e6974a962fbfa
Sijan Paudel <sijanpaudel44@gmail.com>
2025-10-22 00:46:45 +0545

    Refactor ProductImage model to improve image field formatting and add FileExtensionValidator for future use

--- store/models.py
+++ store/models.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.conf import settings
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, FileExtensionValidator
 from django.db import models
 from uuid import uuid4
 
@@ -49,8 +49,10 @@ class ProductImage(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name='images'
     )
-    image = models.ImageField(upload_to='store/images'
-                              , validators=[validate_file_size])
+    image = models.ImageField(upload_to='store/images',
+                              validators=[validate_file_size])
+    # image = models.FileField(upload_to='store/images'
+    #                           , validators=[FileExtensionValidator(allowed_extensions=['pdf'])])
 
 
 class Customer(models.Model):
@@ -110,7 +112,8 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='items')
+    order = models.ForeignKey(
+        Order, on_delete=models.PROTECT, related_name='items')
     product = models.ForeignKey(
         Product, on_delete=models.PROTECT, related_name='orderitems')
     quantity = models.PositiveSmallIntegerField()
