986196a94d9f08dc998d4893b8588c2248e61b40
Sijan Paudel <sijanpaudel44@gmail.com>
2025-10-22 02:34:56 +0545

    Add django-templated-mail package and implement email sending functionality with templated email

--- playground/views.py
+++ playground/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.core.mail import EmailMessage, BadHeaderError
 # To prevent from interception, we use BadHeaderError while sending email so that no one can inject headers in the email.
 
+from templated_mail.mail import BaseEmailMessage
+
 
 def say_hello(request):
     try:
@@ -11,9 +13,18 @@ def say_hello(request):
         # # this is for general email sending
         # send_mail('subject', 'message',
         #           'info@sijanbuy.com', ['bob@sijanbuy.com'])
-        message = EmailMessage('subject','message','sijan@buy.com',['hi@buy.com', 'hello@buy.com','hihello@buy.com'])
-        message.attach_file('playground/static/images/image.png')
-        message.send()
+        # message = EmailMessage('subject','message','sijan@buy.com',['hi@buy.com', 'hello@buy.com','hihello@buy.com'])
+        # message.attach_file('playground/static/images/image.png')
+        # message.send()
+
+        message = BaseEmailMessage(
+            template_name='emails/hello.html',
+            context={
+                'name': "Sijan"
+            }
+        )
+        message.send(['hello@buy.com'])
+
     except BadHeaderError:
         pass
     return render(request, 'hello.html', {'name': 'Mosh'})
