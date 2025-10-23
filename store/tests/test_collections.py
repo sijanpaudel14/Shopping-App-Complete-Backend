b2283ab3e64698ffa01b0ced349d9629f31cde08
Sijan Paudel <sijanpaudel44@gmail.com>
2025-10-23 23:01:53 +0545

    Add pytest configuration and initial test for collection creation

---
+++ store/tests/test_collections.py
@@ -0,0 +1,23 @@
+from rest_framework.test import APIClient
+from rest_framework import status
+import pytest
+
+
+@pytest.mark.django_db
+class TestCreateCollection:
+
+    def test_if_user_is_anonymous_returns_401(self):
+        # Test that an anonymous user cannot create a collection
+        # AAA, (Arrange, Act, Assert)
+        # In Arange part, we setup the test client without authentication
+        # In Act part, we make a POST request to create a collection
+        # In Assert part, we check that the response status code is 401 Unauthorized
+
+        # Here, Arrange part is empty because the test client is already set up by default as anonymous
+        # For Act part:
+        client = APIClient()
+        response = client.post('/store/collections/',
+                               {'title': 'New Collection'})
+
+        # For Assert part:
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
