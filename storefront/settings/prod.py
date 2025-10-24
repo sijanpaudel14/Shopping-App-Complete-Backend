5af03ad08bed1d3674a2c9ce283ffec82fff7ba4
Sijan Paudel <sijanpaudel44@gmail.com>
2025-10-25 01:14:14 +0545

    Configure production settings, add PostgreSQL support, and set up Azure deployment

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,8 +1,33 @@
+#
 import os
-from .common import * 
+from .common import *
+import dj_database_url
 
-
-DEBUG= False
+DEBUG = False
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS", "sijanbuy-prod.azurewebsites.net").split(",")
 
-ALLOWED_HOSTS = []
+# Azure SQL Database configuration
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD'),
+#         'HOST': os.environ.get('DB_HOST'),
+#         'PORT': os.environ.get("DB_PORT", "3306"),
+#     }
+# }
+
+# PostgreSQL from Render
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
+# Static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- storefront/urls.py
+++ storefront/urls.py
@@ -17,7 +17,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-import debug_toolbar
+# import debug_toolbar
 
 admin.site.site_header = 'Storefront Admin'
 admin.site.index_title = 'Admin'
@@ -29,7 +29,7 @@ urlpatterns = [
     path('store/', include('store.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-    path('__debug__/', include(debug_toolbar.urls)),
+    # path('__debug__/', include(debug_toolbar.urls)),
 ]
 
 if settings.DEBUG:
