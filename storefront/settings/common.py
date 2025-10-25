a6d4c064897caed1ac4c640e7e3b0005e4af8cc0
Sijan Paudel <sijanpaudel44@gmail.com>
2025-10-25 15:04:45 +0545

    Update CORS allowed origins by removing trailing slashes from URLs

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -126,8 +126,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://localhost:8001',
     'http://127.0.0.1:8001',
-    'https://buywithsijan.vercel.app/',
-    'https://sijanbuy.onrender.com/'
+    'https://buywithsijan.vercel.app',
+    'https://sijanbuy.onrender.com'
 ]
 
 # Allow credentials for JWT authentication
